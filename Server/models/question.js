2a2c1fd841c22a7e82a75cceee7b1e01f1352185
JSWhiteOG <jswhite@gmail.com>
2024-01-23 20:17:20 -0800

    config'd sql

---
+++ Server/models/question.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Question extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Question.init({
+    question_id: DataTypes.INTEGER,
+    question: DataTypes.STRING,
+    answer: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Question',
+  });
+  return Question;
+};
